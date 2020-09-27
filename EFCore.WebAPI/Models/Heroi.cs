2b7e7c7b8697ab56436fc665534cdfe5f1d87c45
reginaldosobrinho <67155144+reginaldosobrinho@users.noreply.github.com>
2020-09-27 13:39:16 -0300

    Add project files.

---
+++ EFCore.WebAPI/Models/Heroi.cs
@@ -0,0 +1,18 @@
+﻿using EFCore.WebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCore.WebAPI
+{
+    public class Heroi
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public IdentidadeSecreta Identidade { get; set; }
+        public List<Arma> Armas { get; set; }
+        public List<HeroiBatalha> HeroisBatalhas { get; set; }
+    }
+}
+    
